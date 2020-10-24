ba4e22347b3ea1a37a5f9410d3ad13e5982d04b5
Jimmy Chu <jimmychu0807@gmail.com>
2020-10-24 10:03:50 +0800

    only 1 error left about lifetime

--- src/heartbeat.rs
+++ src/heartbeat.rs
@@ -1,5 +1,6 @@
 use std::{
 	boxed::Box,
+	marker::{Unpin, Send},
 	time::Instant,
 };
 
@@ -10,10 +11,11 @@ use core::{
 	time::{Duration},
 };
 use tokio::{
-	stream::{Stream},
+	stream::{Stream, StreamExt},
 };
 use futures_timer::Delay;
 
+type VS = Vec<Box<dyn Stream<Item = char> + Unpin + Send>>;
 const heartbeat_imported: Instant = Instant::now();
 
 pub struct HeartbeatStream {
@@ -21,13 +23,12 @@ pub struct HeartbeatStream {
 	heartbeat: Option<Duration>,
 	next_heartbeat: Option<Delay>,
 	last_signal: Option<Instant>,
-	streams: Vec<Box<dyn Stream<Item = char>>>,
+	streams: VS,
 	char: char,
 }
 
 impl HeartbeatStream {
-	pub fn new(cooldown_sec: Option<u64>, heartbeat_sec: Option<u64>,
-		streams: Vec<Box<dyn Stream<Item = char>>>, char: char) -> Self
+	pub fn new(cooldown_sec: Option<u64>, heartbeat_sec: Option<u64>, streams: VS, char: char) -> Self
 	{
 		Self {
 			cooldown: cooldown_sec.map(|s| Duration::from_secs(s)),
@@ -49,32 +50,51 @@ impl Stream for HeartbeatStream {
 	type Item = char;
 
 	fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-		let mut stream1 = self.streams[0].as_mut();
-		let mut stream2 = self.streams[1].as_mut();
+		let mut stream1 = self.streams[0];
+		let mut stream2 = self.streams[1];
 
 		let current = Instant::now();
 		let since_last_signal = Instant::now().saturating_duration_since(
 			self.last_signal.unwrap_or(heartbeat_imported));
 
-		// We ensure we first poll the `next_heartbeat` delay future
-		if let Some(hb) = self.next_heartbeat { Pin::new(&mut hb).poll(cx); }
-
-		tokio::spawn(async {
-			tokio::select! {
-				char = stream1, if self.cooldown.map_or(true, |cd| since_last_signal > cd) => {
-					self.reset_last_signal_next_heartbeat();
-					return Poll::Ready(char)
-				},
-				char = stream2, if self.cooldown.map_or(true, |cd| since_last_signal > cd) => {
-					self.reset_last_signal_next_heartbeat();
-					return Poll::Ready(char)
-				},
-				_ = self.next_heartbeat => {
-					self.reset_last_signal_next_heartbeat();
-					return Poll::Ready(self.char)
-				}
+		// Dealing with heartbeat and no heartbeat
+		match &mut self.next_heartbeat {
+			Some(hb) => {
+				Pin::new(hb).poll(cx);
+				tokio::spawn(async move {
+					tokio::select! {
+						Some(char) = stream1.next(), if self.cooldown.map_or(true, |cd| since_last_signal > cd) => {
+							self.reset_last_signal_next_heartbeat();
+							return Poll::Ready(Some(char))
+						},
+						Some(char) = stream2.next(), if self.cooldown.map_or(true, |cd| since_last_signal > cd) => {
+							self.reset_last_signal_next_heartbeat();
+							return Poll::Ready(Some(char))
+						},
+						_ = hb => {
+							self.reset_last_signal_next_heartbeat();
+							return Poll::Ready(Some(self.char))
+						},
+						else => Poll::Ready(None),
+					}
+				});
+			},
+			None => {
+				tokio::spawn(async move {
+					tokio::select! {
+						Some(char) = stream1.next(), if self.cooldown.map_or(true, |cd| since_last_signal > cd) => {
+							self.reset_last_signal_next_heartbeat();
+							return Poll::Ready(Some(char))
+						},
+						Some(char) = stream2.next(), if self.cooldown.map_or(true, |cd| since_last_signal > cd) => {
+							self.reset_last_signal_next_heartbeat();
+							return Poll::Ready(Some(char))
+						},
+						else => Poll::Ready(None),
+					}
+				});
 			}
-		});
+		}
 
 		Poll::Pending
 	}
